d38fb2614587ca42dfaa4e290a2f0c19e698242b
Mihai Carabas <mihai.carabas@gmail.com>
2012-06-18 17:55:58 +0300

    sys/platform/xxx/include/smp.h: some clean-up for non-SMP compilation

--- sys/platform/vkernel64/include/smp.h
+++ sys/platform/vkernel64/include/smp.h
@@ -183,13 +183,6 @@ int get_logical_CPU_number_within_core(int cpuid);
 
 #define	smp_active_mask	1	/* smp_active_mask always 1 on UP machines */
 
-/* Non-SMP systems - unique core id 0 */
-#define detect_cpu_topology
-#define get_chip_ID(cpuid) 0
-#define get_core_number_within_chip(cpuid) 0
-#define get_logical_CPU_number_within_core(cpuid) 0
-#define get_cpuid_from_apicid(cpuid) 0
-
 #endif
 
 #endif /* _KERNEL */
