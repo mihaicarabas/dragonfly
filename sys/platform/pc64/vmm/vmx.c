c013b106042551423ecef1c33d5ea3d6a5aa5770
Mihai Carabas <mihai.carabas@gmail.com>
2013-06-24 23:49:35 +0300

    sys: platform: pc64: vmm: add init code in order to execute vmxon

--- sys/platform/pc64/vmm/vmx.c
+++ sys/platform/pc64/vmm/vmx.c
@@ -1,16 +1,300 @@
+#include <sys/malloc.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/types.h>
+#include <sys/thread.h>
+#include <sys/thread2.h>
+
+#include <machine/cpufunc.h>
+#include <machine/cputypes.h>
+#include <machine/specialreg.h>
+#include <machine/smp.h>
+#include <machine/globaldata.h>
+
 #include "vmm.h"
 
 #include "vmx.h"
+#include "vmx_instr.h"
+
+struct vmx_ctl_info vmx_pinbased = {
+	.msr_addr = IA32_VMX_PINBASED_CTLS,
+	.msr_true_addr = IA32_VMX_TRUE_PINBASED_CTLS,
+};
+
+struct vmx_ctl_info vmx_procbased = {
+	.msr_addr = IA32_VMX_PROCBASED_CTLS,
+	.msr_true_addr = IA32_VMX_TRUE_PROCBASED_CTLS,
+};
+
+struct vmx_ctl_info vmx_procbased2 = {
+	.msr_addr = IA32_VMX_PROCBASED_CTLS2,
+	.msr_true_addr = IA32_VMX_PROCBASED_CTLS2,
+};
+
+struct vmx_ctl_info vmx_exit = {
+	.msr_addr = IA32_VMX_EXIT_CTLS,
+	.msr_true_addr = IA32_VMX_TRUE_EXIT_CTLS,
+};
+
+struct vmx_ctl_info vmx_entry = {
+	.msr_addr = IA32_VMX_ENTRY_CTLS,
+	.msr_true_addr = IA32_VMX_TRUE_ENTRY_CTLS,
+};
+
+struct vmx_ctl_info vmx_ept_vpid_cap = {
+	.msr_addr = IA32_VMX_EPT_VPID_CAP,
+	.msr_true_addr = IA32_VMX_EPT_VPID_CAP,
+};
+
+
+
+struct vmx_pcpu_info *pcpu_info;
+
+/* VMX set control setting
+ * Intel System Programming Guide, Part 3, Order Number 326019
+ * 31.5.1 Algorithms for Determining VMX Capabilities
+ * Implement Algorithm 3
+ */
+static int
+vmx_set_ctl_setting(struct vmx_ctl_info *vmx_ctl, uint32_t bit_no, setting_t value) {
+	uint64_t vmx_basic;
+	uint64_t ctl_val;
+
+	/* Check if its branch b. or c. */
+	vmx_basic = rdmsr(IA32_VMX_BASIC);
+	if (IS_TRUE_CTL_AVAIL(vmx_basic))
+		ctl_val = rdmsr(vmx_ctl->msr_true_addr);
+	else
+		ctl_val = rdmsr(vmx_ctl->msr_addr);
+	
+	/* Check if the value is known by VMM or set on DEFAULT */
+	switch(value) {
+		case DEFAULT:
+			/* Both settings are allowd
+			 * - step b.iii)
+			 *   or
+			 * - c.iii), c.iv)
+			 */
+			if (IS_ZERO_SETTING_ALLOWED(ctl_val, bit_no)
+			    && IS_ONE_SETTING_ALLOWED(ctl_val, bit_no)) {
+
+				/* For c.iii) and c.iv) */
+				if(IS_TRUE_CTL_AVAIL(vmx_basic))
+					ctl_val = rdmsr(vmx_ctl->msr_addr);
+
+				if (IS_ZERO_SETTING_ALLOWED(ctl_val, bit_no))
+					vmx_ctl->ctls |= ~BIT(bit_no);
+				if (IS_ONE_SETTING_ALLOWED(ctl_val, bit_no))
+					vmx_ctl->ctls &= BIT(bit_no);
+
+			} else if (IS_ZERO_SETTING_ALLOWED(ctl_val, bit_no)) {
+				/* b.i), c.i) */
+				vmx_ctl->ctls |= ~BIT(bit_no);
+			} else if (IS_ONE_SETTING_ALLOWED(ctl_val, bit_no)) {
+				/* b.i), c.i) */
+				vmx_ctl->ctls &= BIT(bit_no);
+			} else {
+				return (EINVAL);
+			}
+			break;
+		case ZERO:
+			/* For b.ii) or c.ii) */
+			if (!IS_ZERO_SETTING_ALLOWED(ctl_val, bit_no))
+				return (EINVAL);
+
+			vmx_ctl->ctls |= ~BIT(bit_no);
+
+			break;
+		case ONE:
+			/* For b.ii) or c.ii) */
+			if (IS_ONE_SETTING_ALLOWED(ctl_val, bit_no))
+				return (EINVAL);
+
+			vmx_ctl->ctls &= BIT(bit_no);
+
+			break;
+	}
+	return 0;
+}
+
+static void
+vmx_set_default_settings(struct vmx_ctl_info *vmx_ctl)
+{
+	int i;
+
+	for(i = 0; i < 32; i++) {
+		vmx_set_ctl_setting(vmx_ctl, i, DEFAULT);
+	}
+}
+
+static void
+alloc_vmxon_regions(void)
+{
+	uint64_t vmx_basic_value;
+	int cpu;
+
+	vmx_basic_value = rdmsr(IA32_VMX_BASIC);
+	pcpu_info = kmalloc(ncpus * sizeof(struct vmx_pcpu_info), M_TEMP, M_WAITOK | M_ZERO);
+
+	kprintf("alloc_vmxon_regions: VMX_WIDHTH_ADDR: %x\n", (unsigned int)VMX_WIDTH_ADDR(vmx_basic_value));
+
+	for (cpu = 0; cpu < ncpus; cpu++) {
+
+		/* The address must be aligned to 4K - alloc extra */
+		pcpu_info[cpu].vmxon_region = kmalloc(VMX_REGION(vmx_basic_value) + VMXON_REGION_ALIGN_SIZE,
+		    M_TEMP,
+		    M_WAITOK | M_ZERO);
+
+		/* Align address */
+		pcpu_info[cpu].vmxon_region_aligned = (unsigned char*) VMXON_REGION_ALIGN(pcpu_info[cpu].vmxon_region);
+
+		/* In the first 31 bits put the vmx revision*/
+		*((uint32_t *) pcpu_info[cpu].vmxon_region_aligned) = VMX_REVISION(vmx_basic_value);
+	}
+}
+
+static void
+free_vmxon_regions(void)
+{
+	int i;
+
+	for (i = 0; i < ncpus; i++) {
+		pcpu_info[i].vmxon_region_aligned = NULL;
+
+		kfree(pcpu_info[i].vmxon_region, M_TEMP);
+	}
+
+	kfree(pcpu_info, M_TEMP);
+}
+
+static void
+execute_vmxon(void *dummy)
+{
+	int err;
+	uint64_t cr0_fixed_bits_to_1;
+	uint64_t cr0_fixed_bits_to_0;
+	uint64_t cr4_fixed_bits_to_0;
+	uint64_t cr4_fixed_bits_to_1;
+	unsigned char *vmxon_region;
+
+	/* A.7 VMX-FIXED BITS IN CR0 */
+	cr0_fixed_bits_to_1 = rdmsr(IA32_VMX_CR0_FIXED0);
+	cr0_fixed_bits_to_0 = rdmsr(IA32_VMX_CR0_FIXED1);
+	load_cr0(rcr0() | (cr0_fixed_bits_to_1 & cr0_fixed_bits_to_0));
+
+	/* A.8 VMX-FIXED BITS IN CR4 */
+	cr4_fixed_bits_to_1 = rdmsr(IA32_VMX_CR0_FIXED0);
+	cr4_fixed_bits_to_0 = rdmsr(IA32_VMX_CR0_FIXED1);
+	load_cr4(rcr4() | (cr4_fixed_bits_to_1 & cr4_fixed_bits_to_0));
+
+	/* Enable VMX */
+	load_cr4(rcr4() | CR4_VMXE);
+
+	vmxon_region = pcpu_info[mycpuid].vmxon_region_aligned;
+	err = vmxon(vmxon_region);
+	if (err) {
+		kprintf("vmxon failed on cpu%d\n", mycpuid);
+	}
+}
+
+static void
+execute_vmxoff(void *dummy)
+{
+	vmxoff();
+
+	/* Disable VMX */
+	load_cr4(rcr4() & ~CR4_VMXE);
+}
 
 static int
 vmx_init(void)
 {
+	uint64_t feature_control;
+	int err;
+	globaldata_t gd;
+	int cpu, seq;
+
+
+	/*
+	 * The ability of a processor to support VMX operation
+	 * and related instructions is indicated by:
+	 * CPUID.1:ECX.VMX[bit 5] = 1
+	 */
+	if (!(cpu_feature2 & CPUID2_VMX)) {
+		kprintf("VMX is not supported by this Intel CPU\n");
+		return (ENODEV);
+	}
+
+	vmx_set_default_settings(&vmx_pinbased);
+	vmx_set_default_settings(&vmx_procbased);
+	vmx_set_default_settings(&vmx_procbased2);
+	vmx_set_default_settings(&vmx_exit);
+	vmx_set_default_settings(&vmx_entry);
+
+	/* Enable second level for procbades */
+	err = vmx_set_ctl_setting(&vmx_procbased,
+	    PROCBASED_ACTIVATE_SECONDARY_CONTROLS,
+	    ONE);
+	if (err) {
+		kprintf("PROCBASED_ACTIVATE_SECONDARY_CONTROLS not supported by this CPU\n");
+		return (ENODEV);
+	}
+
+	/* Enable EPT feature */
+	err = vmx_set_ctl_setting(&vmx_procbased2,
+	    PROCBASED2_ENABLE_EPT,
+	    ONE);
+	if (err) {
+		kprintf("PROCBASED2_ENABLE_EPT not supported by this CPU\n");
+		return (ENODEV);
+	}
+
+	/* Enable VPID feature */
+	err = vmx_set_ctl_setting(&vmx_procbased2,
+	    PROCBASED2_ENABLE_VPID,
+	    ONE);
+	if (err) {
+		kprintf("PROCBASED2_ENABLE_VPID not supported by this CPU\n");
+		return (ENODEV);
+	}
+
+
+	/* Check for the feature control status */
+	feature_control = rdmsr(IA32_FEATURE_CONTROL);
+	if ((feature_control & FEATURE_CONTROL_LOCKED)) {
+		kprintf("IA32_FEATURE_CONTROl is not locked\n");
+		return (EINVAL);
+	}
+	if (!(feature_control & FEATURE_CONTROL_VMX_BIOS_ENABLED)) {
+		kprintf("VMX is disable by the BIOS\n");
+		return (EINVAL);
+	}
+	
+	alloc_vmxon_regions();
+
+	for (cpu = 0; cpu < ncpus; cpu++) {
+		gd = globaldata_find(cpu);
+		seq = lwkt_send_ipiq(gd, execute_vmxon, NULL);
+		lwkt_wait_ipiq(gd, seq);
+	}
+
+
 	return 0;
 }
 
 static int
 vmx_clean(void)
 {
+	globaldata_t gd;
+	int cpu, seq;
+
+	for (cpu = 0; cpu < ncpus; cpu++) {
+		gd = globaldata_find(cpu);
+		seq = lwkt_send_ipiq(gd, execute_vmxoff, NULL);
+		lwkt_wait_ipiq(gd, seq);
+	}
+
+	free_vmxon_regions();
 	return 0;
 }
 
--- sys/platform/pc64/vmm/vmx.h
+++ sys/platform/pc64/vmm/vmx.h
@@ -2,120 +2,163 @@
 #define _VMM_VMX_H_
 
 
+
+#define BIT(x)	(1ULL << x)
+
+
+/* VMX info for a generic control */
+struct vmx_ctl_info {
+	uint32_t msr_addr;
+	uint32_t msr_true_addr;
+	uint32_t ctls;
+};
+
+/* The value of a setting */
+typedef enum {
+	ZERO,
+	ONE,
+	DEFAULT
+} setting_t;
+
+/* VMX per cpu info */
+struct vmx_pcpu_info {
+	unsigned char *vmxon_region;
+	unsigned char *vmxon_region_aligned;
+
+};
+
+
+
 /*
- * Chapter 30 VMX Instruction Reference
- * Section 30.3 "Conventions"
- * from Intel Architecture Manual 3C.
+ * MSR register address
  */
-#define	VM_SUCCEED		0
-#define	VM_FAIL_INVALID		1
-#define	VM_FAIL_VALID		2
-#define	GET_ERROR_CODE				\
-	"check_CF:	jnc check_ZF;"		\
-	"		mov $1, %[err];"	\
-	"		jmp ok;"		\
-	"check_ZF:	jnz ok;"		\
-	"		mov $2, %[err];"	\
-	"		jmp end;"		\
-	"ok:		mov $0, %[err];		\
-	"end:"
-#endif
 
-static inline int
-vmxon(char *vmx_region)
-{
-	int err;
-	uint64_t paddr;
-
-	paddr = vtophys(vmx_region);
-	__asm __volatile("vmxon %[paddr];"
-			 GET_ERROR_CODE
-			 : [err] "=r" (err)
-			 : [paddr] "m" (paddr)
-			 : "memory");
-
-	return err;
-}
-
-static inline void
-vmxoff(void)
-{
-
-	__asm __volatile("vmxoff");
-}
-
-static inline void
-vmptrst(uint64_t *addr)
-{
-
-	__asm __volatile("vmptrst %[addr]"
-			:
-			: [addr] "m" (*addr)
-			: "memory");
-}
-
-static inline int
-vmptrld(struct char *vmcs)
-{
-	int err;
-	uint64_t paddr;
-
-	paddr = vtophys(vmcs);
-	__asm __volatile("vmptrld %[addr];"
-			 GET_ERROR_CODE
-			 : [err] "=r" (err)
-			 : [addr] "m" (paddr)
-			 : "memory");
-	return err;
-}
-
-static inline int
-vmwrite(uint64_t reg, uint64_t val)
-{
-	int err;
-
-	__asm __volatile("vmwrite %[val], %[reg];"
-			 GET_ERROR_CODE
-			 : [err] "=r" (err)
-			 : [val] "r" (val), [reg] "r" (reg)
-			 : "memory");
-
-	return err;
-}
-
-static inline int
-vmread(uint64_t reg, uint64_t *addr)
-{
-	int err;
-
-	__asm __volatile("vmread %[r], %[addr];"
-			 GET_ERROR_CODE
-			 : [err] "=r" (err)
-			 : [reg] "r" (reg), [addr] "m" (*addr)
-			 : "memory");
-
-	return err;
-}
+#define		IA32_FEATURE_CONTROL			0x3A
+#define		IA32_VMX_BASIC				0x480
+#define		IA32_VMX_PINBASED_CTLS			0x481
+#define		IA32_VMX_PROCBASED_CTLS			0x482
+#define		IA32_VMX_EXIT_CTLS			0x483
+#define		IA32_VMX_ENTRY_CTLS			0x484
+#define		IA32_VMX_CR0_FIXED0			0x486
+#define		IA32_VMX_CR0_FIXED1			0x487
+#define		IA32_VMX_CR4_FIXED0			0x488
+#define		IA32_VMX_CR4_FIXED1			0x489
+#define		IA32_VMX_EPT_VPID_CAP			0x48C
+#define		IA32_VMX_PROCBASED_CTLS2		0x48B
+#define		IA32_VMX_TRUE_PINBASED_CTLS		0x48D
+#define		IA32_VMX_TRUE_PROCBASED_CTLS		0x48E
+#define		IA32_VMX_TRUE_EXIT_CTLS			0x48F
+#define		IA32_VMX_TRUE_ENTRY_CTLS		0x490
 
 
 
-/* MSR registers */
 
-#define		IA32_VMX_BASIC			0x480
-#define		IA32_VMX_PINBASED_CTLS		0x481
-#define		IA32_VMX_PROCBASED_CTLS		0x482
-#define		IA32_VMX_EXIT_CTLS		0x483
-#define		IA32_VMX_ENTRY_CTLS		0x484
-#define		IA32_VMX_CR0_FIXED0		0x486
-#define		IA32_VMX_CR0_FIXED1		0x487
-#define		IA32_VMX_CR4_FIXED0		0x488
-#define		IA32_VMX_CR4_FIXED1		0x489
-#define		IA32_VMX_TRUE_ENTRY_CTLS	0x490
-#define		IA32_VMX_PROCBASED_CTLS2	0x48B
-#define		IA32_VMX_EPT_VPID_CAP		0x48C
-#define		IA32_VMX_TRUE_PINBASED_CTLS	0x48D
-#define		IA32_VMX_TRUE_PROCBASED_CTLS	0x48E
-#define		IA32_VMX_TRUE_EXIT_CTLS		0x48F
+/*
+ * IA32 FEATURE CONTROL bits
+ */
+#define		FEATURE_CONTROL_LOCKED			0
+#define		FEATURE_CONTROL_VMX_BIOS_ENABLED	2
+
+
+
+/*
+ * IA32_VMX_BASIC
+ * A.1 BASIC VMX INFORMATION
+ */
+#define		IS_TRUE_CTL_AVAIL(VMX_BASIC)	((VMX_BASIC) & (1ULL << (55)))
+#define		VMX_REVISION(reg_val)		(reg_val &0x7fffffff) /* 0:30 */
+#define 	VMX_REGION(reg_val)		((reg_val >> 32) &0x01ffffff) /* 32:44 */
+#define 	VMX_WIDTH_ADDR(reg_val)		(reg_val >> 48 & 0x1) /* 48 */
+#define		VMXON_REGION_ALIGN_SIZE		4096
+#define		VMXON_REGION_ALIGN(p)		(((unsigned long)(p) + VMXON_REGION_ALIGN_SIZE) & ~VMXON_REGION_ALIGN_SIZE)
+
+
+/*
+ * Pin-Based VM-Execution Controls
+ * Table 24-5. Definitions of Pin-Based Controls
+ * */
+#define		PINBASED_EXTERNAL_INTERRUPT_EXITING	0
+#define		PINBASED_NMI_EXITING			3
+#define		PINBASED_VIRTUAL_NMIS			5
+#define		PINBASED_ACTIVATE_VMX_PREEMPTIOn_TIEMR	6
+#define		PINBASED_PROCESS_POSTED_INTERRUPTS	7
+
+
+
+/*
+ * Processor-Based VM-Execution Controls
+ * Table 24-6. Definitions of Primary Processor-Based Controls
+ */
+#define		PROCBASED_INTERRUPT_WINDOW_EXITING	2
+#define		PROCBASED_USE_TSC_OFFSETING		3
+#define		PROCBASED_HLT_OFFSETING			7
+#define		PROCBASED_INVLPG_EXITING		9
+#define		PROCBASED_MWAIT_EXITING			10
+#define		PROCBASED_RDPMC_EXITING			11
+#define		PROCBASED_RDTSC_EXITING			12
+#define		PROCBASED_CR3_LOAD_EXITING		15
+#define		PROCBASED_CR3_STORE_EXITING		16
+#define		PROCBASED_CR8_LOAD_EXITING		19
+#define		PROCBASED_CR8_STORE_EXITING		20
+#define		PROCBASED_USE_TPR_SHADOW		21
+#define		PROCBASED_NMI_WINDOWS_EXITING		22
+#define		PROCBASED_MOV_DR_EXITING		23
+#define		PROCBASED_UNCOND_IO_EXITING		24
+#define		PROCBASED_USE_IO_BITMAPS		25
+#define		PROCBASED_MONITOR_TRAP_FLAG		27
+#define		PROCBASED_USE_MSR_BITMAPS		28
+#define		PROCBASED_MONITOR_EXITING		29
+#define		PROCBASED_PAUSE_EXITING			30
+#define		PROCBASED_ACTIVATE_SECONDARY_CONTROLS	31
+/* Table 24-7. Definitions of Secondary Processor-Based Controls */
+#define		PROCBASED2_VIRTUALIZE_APIC_ACCESSES	0
+#define		PROCBASED2_ENABLE_EPT			1
+#define		PROCBASED2_DESCRIPTOR_TABLE_EXITING	2
+#define		PROCBASED2_ENABLE_RDTSCP		3
+#define		PROCBASED2_VIRTUAL_x2APIC_MODE		4
+#define		PROCBASED2_ENABLE_VPID			5
+#define		PROCBASED2_WBINVD_EXITING		6
+#define		PROCBASED2_UNRESTRICTED_UEST		7
+#define		PROCBASED2_APIC_REGISTER_VIRTULIZATION	8
+#define		PROCBASED2_VIRTUAL_INTERRUPT_DELIVERY	9
+#define		PROCBASED2_PAUSE_LOOP_EXITING		10
+#define		PROCBASED2_RDRAND_EXITING		11
+#define		PROCBASED2_ENABLE_INVPCID		12
+#define		PROCBASED2_ENABLE_VM_FUNCTIONS		13
+#define		PROCBASED2_VMCS_SHADOWING		14
+#define		PROCBASED2_EPT_VIOLATION_VE		18
+
+
+
+/*
+ * VM-EXIT CONTROL FIELDS
+ * Table 24-10. Definitions of VM-Exit Controls
+ */
+#define		VMEXIT_SAVE_DEBUG_CONTROLS		2
+#define		VMEXIT_HOST_ADDRESS_SPACE_SIZE		9
+#define		VMEXIT_LOAD_IA32_PERF_GLOBAL_CTRL	12
+#define		VMEXIT_ACKNOWLEDGE_INTERRUPT_ON_EXIT	15
+#define		VMEXIT_SAVE_IA32_PAT			18
+#define		VMEXIT_LOAD_IA32_PAT			19
+#define		VMEXIT_SAVE_IA32_EFER_PAT		20
+#define		VMEXIT_LOAD_IA32_EFER_PAT		21
+#define		VMEXIT_SAVE_VMX_PREEMPTION_TIMER	22
+
+
+
+/*
+ * VM-ENTRY CONTROL FIELDS
+ * Table 24-12. Definitions of VM-Entry Controls
+ */
+#define		VMENTRY_LOAD_DEBUG_CONTROLS		2
+#define		VMENTRY_IA32e_mode_guest		9
+#define		VMENTRY_ENTRY_TO_SMM			10
+#define		VMENTRY_DEACTIVATE_DUAL_MONITOR		11
+#define		VMENTRY_LOAD_IA32_PERF_GLOBAL_CTRL	13
+#define		VMENTRY_LOAD_IA32_PAT			14
+#define		VMENTRY_LOAD_IA32_EFER			15
+
+
 
 #define IS_ONE_SETTING_ALLOWED(val, bit)	\
     ((val) & (1ULL << (bit + 32)))
@@ -123,11 +166,4 @@ vmread(uint64_t reg, uint64_t *addr)
 #define IS_ZERO_SETTING_ALLOWED(val, bit)	\
     ((val) & (1ULL << (bit)))
 
-/*
- * A.1 BASIC VMX INFORMATION
- * from Intel Architecture Manual 3C.
- */
-#define VMX_REVISION(reg_val)		(reg_val &0x7fffffff)		/* 0:30 */
-#define VMX_REGION(reg_val)		((reg_val >> 32) &0x01ffffff)	/* 32:44 */
-#define VMX_WIDTH_ADDR(reg_val)		(reg_val >> 48 & 0x1)		/* 48 */
-
+#endif
