a9dcbe260d5237fbb28f7a08a37de878c4e02a99
Mihai Carabas <mihai.carabas@gmail.com>
2013-07-01 22:05:43 +0300

    sys: platform: pc64: vmm: vmx.h: add Exit Reasons

--- sys/platform/pc64/vmm/vmx.h
+++ sys/platform/pc64/vmm/vmx.h
@@ -32,7 +32,6 @@ struct vmx_pcpu_info {
 /*
  * MSR register address
  */
-
 #define		IA32_FEATURE_CONTROL			0x3A
 #define		IA32_VMX_BASIC				0x480
 #define		IA32_VMX_PINBASED_CTLS			0x481
@@ -49,7 +48,6 @@ struct vmx_pcpu_info {
 #define		IA32_VMX_TRUE_PROCBASED_CTLS		0x48E
 #define		IA32_VMX_TRUE_EXIT_CTLS			0x48F
 #define		IA32_VMX_TRUE_ENTRY_CTLS		0x490
-
 
 
 
@@ -71,6 +69,7 @@ struct vmx_pcpu_info {
 #define 	VMX_WIDTH_ADDR(reg_val)		(reg_val >> 48 & 0x1) /* 48 */
 #define		VMXON_REGION_ALIGN_SIZE		4096
 #define		VMXON_REGION_ALIGN(p)		(((unsigned long long)(p) + VMXON_REGION_ALIGN_SIZE) & ~(VMXON_REGION_ALIGN_SIZE - 1))
+
 
 
 /*
@@ -165,5 +164,68 @@ struct vmx_pcpu_info {
 
 #define IS_ZERO_SETTING_ALLOWED(val, bit)	\
     ((val) & (1ULL << (bit)))
+
+
+
+/*
+ * VMX Basic Exit Reasons
+ */
+#define		EXIT_REASON_EXCEPTION		0
+#define		EXIT_REASON_EXT_INTR		1
+#define		EXIT_REASON_TRIPLE_FAULT	2
+#define		EXIT_REASON_INIT		3
+#define		EXIT_REASON_SIPI		4
+#define		EXIT_REASON_IO_SMI		5
+#define		EXIT_REASON_SMI			6
+#define		EXIT_REASON_INTR_WINDOW		7
+#define		EXIT_REASON_NMI_WINDOW		8
+#define		EXIT_REASON_TASK_SWITCH		9
+#define		EXIT_REASON_CPUID		10
+#define		EXIT_REASON_GETSEC		11
+#define		EXIT_REASON_HLT			12
+#define		EXIT_REASON_INVD		13
+#define		EXIT_REASON_INVLPG		14
+#define		EXIT_REASON_RDPMC		15
+#define		EXIT_REASON_RDTSC		16
+#define		EXIT_REASON_RSM			17
+#define		EXIT_REASON_VMCALL		18
+#define		EXIT_REASON_VMCLEAR		19
+#define		EXIT_REASON_VMLAUNCH		20
+#define		EXIT_REASON_VMPTRLD		21
+#define		EXIT_REASON_VMPTRST		22
+#define		EXIT_REASON_VMREAD		23
+#define		EXIT_REASON_VMRESUME		24
+#define		EXIT_REASON_VMWRITE		25
+#define		EXIT_REASON_VMXOFF		26
+#define		EXIT_REASON_VMXON		27
+#define		EXIT_REASON_CR_ACCESS		28
+#define		EXIT_REASON_DR_ACCESS		29
+#define		EXIT_REASON_INOUT		30
+#define		EXIT_REASON_RDMSR		31
+#define		EXIT_REASON_WRMSR		32
+#define		EXIT_REASON_INVAL_VMCS		33
+#define		EXIT_REASON_INVAL_MSR		34
+#define		EXIT_REASON_MWAIT		36
+#define		EXIT_REASON_MTF			37
+#define		EXIT_REASON_MONITOR		39
+#define		EXIT_REASON_PAUSE		40
+#define		EXIT_REASON_MCE			41
+#define		EXIT_REASON_TPR			43
+#define		EXIT_REASON_APIC		44
+#define		EXIT_REASON_GDTR_IDTR		46
+#define		EXIT_REASON_LDTR_TR		47
+#define		EXIT_REASON_EPT_FAULT		48
+#define		EXIT_REASON_EPT_MISCONFIG	49
+#define		EXIT_REASON_INVEPT		50
+#define		EXIT_REASON_RDTSCP		51
+#define		EXIT_REASON_VMX_PREEMPT		52
+#define		EXIT_REASON_INVVPID		53
+#define		EXIT_REASON_WBINVD		54
+#define		EXIT_REASON_XSETBV		55
+#define		EXIT_REASON_APIC_WRITE		56
+#define		EXIT_REASON_RDRAND		57
+#define		EXIT_REASON_INVPCID		58
+#define		EXIT_REASON_VMFUNC		59
+
 
 #endif
