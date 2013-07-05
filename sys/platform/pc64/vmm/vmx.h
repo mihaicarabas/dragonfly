6f1c50d50ce1e41e5a591bc8b6dca97a5a9f1f01
Mihai Carabas <mihai.carabas@gmail.com>
2013-07-05 23:49:03 +0300

    sys: platform: pc64: vmm: vmx.h: add registers for state saving

--- sys/platform/pc64/vmm/vmx.h
+++ sys/platform/pc64/vmm/vmx.h
@@ -29,6 +29,36 @@ struct vmx_pcpu_info {
 struct vmx_thread_info {
 	unsigned char *vmcs_region_na;
 	unsigned char *vmcs_region;
+	int launched;
+	int last_cpu;
+	/* Guest unsaved registers in VMCS*/
+	register_t	guest_rax;
+	register_t	guest_rbx;
+	register_t	guest_rcx;
+	register_t	guest_rdx;
+	register_t	guest_rsi;
+	register_t	guest_rdi;
+	register_t	guest_rbp;
+	register_t	guest_r8;
+	register_t	guest_r9;
+	register_t	guest_r10;
+	register_t	guest_r11;
+	register_t	guest_r12;
+	register_t	guest_r13;
+	register_t	guest_r14;
+	register_t	guest_r15;
+	register_t	guest_cr2;
+
+	/* Host unsaved registers in VMCS */
+	register_t	host_r15;
+	register_t	host_r14;
+	register_t	host_r13;
+	register_t	host_r12;
+	register_t	host_rbp;
+	register_t	host_rsp;
+	register_t	host_rbx;
+	register_t	host_rip;
+
 };
 
 
