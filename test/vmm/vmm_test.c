6d30ba9a7d549c9bb051a23a247e4314833171b0
Mihai Carabas <mihai.carabas@gmail.com>
2013-07-10 23:12:39 +0300

    test: vmm: vmm_text.c: comment out all and leave just one call to vmm_guest_ctl

--- test/vmm/vmm_test.c
+++ test/vmm/vmm_test.c
@@ -11,6 +11,16 @@
 
 #define vmm_printf(val, err, exp) \
 	printf("vmm_guest(%d): return %d, expected %d\n", val, err, exp);
+
+void test() {
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	printf("%x %x %x %x %x\n", &a, &b, &c, &d, &e);
+
+}
 int
 main(void)
 {
@@ -19,28 +29,27 @@ main(void)
 	size_t len = 3;
 	int enable = 0;
 	int enabl2 = 0;
-	sysctlnametomib("hw.vmm.enable", mib, &len);
-
-	error = vmm_guest_ctl(100);
-	vmm_printf(100, error, -1);
+//	sysctlnametomib("hw.vmm.enable", mib, &len);
+//	test();
+//	error = vmm_guest_ctl(100);
+//	vmm_printf(100, error, -1);
 
 	/* Disable vmm and make the vmm_guest syscall */
-	printf ("hw.vmm.enable = 0\n");
-	len = sizeof(int);
-	if (sysctl(mib, 3, &enabl2, &len, &enable, len) == -1)
-		perror("sysctl");
+//	printf ("hw.vmm.enable = 0\n");
+//	len = sizeof(int);
+//	if (sysctl(mib, 3, &enabl2, &len, &enable, len) == -1)
+//		perror("sysctl");
 
-	error = vmm_guest_ctl(VMM_GUEST_INIT);
-	vmm_printf(VMM_GUEST_INIT, error, -1);
+//	error = vmm_guest_ctl(VMM_GUEST_INIT);
+//	vmm_printf(VMM_GUEST_INIT, error, -1);
 
 	/* Enable vmm and make the vmm_guest syscall */
-	printf ("hw.vmm.enable = 1\n");
-	enable = 1;
-	if (sysctl(mib, 3, NULL, NULL, &enable, sizeof(int)) == -1)
-		perror("sysctl");
+//	printf ("hw.vmm.enable = 1\n");
+//	enable = 1;
+//	if (sysctl(mib, 3, NULL, NULL, &enable, sizeof(int)) == -1)
+//		perror("sysctl");
 
 	error = vmm_guest_ctl(VMM_GUEST_INIT);
 	vmm_printf(VMM_GUEST_INIT, error, 0);
-
 	return 0;
 }
