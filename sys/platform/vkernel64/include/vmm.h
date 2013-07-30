bb7acf1e1193a41efac125c44d97dad37445bbd6
Mihai Carabas <mihai.carabas@gmail.com>
2013-07-30 22:24:58 +0300

    sys: platform: pc32/vkernel/vkernel64: vmm.h: add stub functions

--- sys/platform/vkernel64/include/vmm.h
+++ sys/platform/vkernel64/include/vmm.h
@@ -1,8 +1,29 @@
 #ifndef _MACHINE_VMM_H_
 #define _MACHINE_VMM_H_
 
-#define vmm_vminit(x) -1
-#define vmm_vmdestroy() -1
-#define vmm_vmrun() -1
+#include <sys/vmm_guest_ctl.h>
 
+static inline int
+vmm_vminit(struct guest_options *go)
+{
+	return -1;
+}
+
+static inline int
+vmm_vmdestroy(void)
+{
+	return -1;
+}
+
+static inline int
+vmm_vmrun(void)
+{
+	return -1;
+}
+
+static inline int
+vmm_vm_set_tls_area(void)
+{
+	return -1;
+}
 #endif
