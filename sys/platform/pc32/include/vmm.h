20873f5f10960be11e0b568c01f4c27df59637e2
Mihai Carabas <mihai.carabas@gmail.com>
2013-09-12 12:39:28 +0300

    sys: platform: *: include: vmm.h: add stubs for all archs

--- sys/platform/pc32/include/vmm.h
+++ sys/platform/pc32/include/vmm.h
@@ -3,27 +3,32 @@
 
 #include <sys/vmm_guest_ctl.h>
 
-static inline int
-vmm_vminit(struct guest_options *go)
-{
-	return -1;
+static __inline
+int vmm_vminit(struct guest_options*) {
+	return 0;
 }
 
-static inline int
-vmm_vmdestroy(void)
-{
-	return -1;
+static __inline
+int vmm_vmdestroy(void) {
+	return 0;
 }
 
-static inline int
-vmm_vmrun(void)
-{
-	return -1;
+static __inline
+int vmm_vmrun(void) {
+	return 0;
 }
 
-static inline int
-vmm_vm_set_tls_area(void)
-{
-	return -1;
+static __inline
+int vmm_vm_set_tls_area(void) {
+	return 0;
 }
+
+static __inline
+void vmm_lwp_return(struct lwp *lp, struct trapframe *frame) {
+}
+
+static __inline
+void vmm_vm_set_guest_cr3(register_t guest_cr3) {
+}
+
 #endif
--- sys/platform/pc64/include/vmm.h
+++ sys/platform/pc64/include/vmm.h
@@ -8,5 +8,7 @@ int vmm_vmdestroy(void);
 int vmm_vmrun(void);
 int vmm_vm_set_tls_area(void);
 void vmm_lwp_return(struct lwp *lp, struct trapframe *frame);
+void vmm_vm_set_guest_cr3(register_t guest_cr3);
+int vmm_vm_get_gpa(register_t *gpa, register_t uaddr);
 
 #endif
